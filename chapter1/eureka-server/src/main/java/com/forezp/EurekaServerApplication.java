0201e701ddaf21df20c9682a79264b1f76bffd42
liuhu <liuhu-jk@360jinrong.net>
2018-12-24 11:37:48 +0800

    34

--- chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
-
 	public static void main(String[] args) {
+		System.out.println("12345");
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
