4e84d395fcd95e2ec7cb455e1a19731405e69106
liuhu <liuhu-jk@360jinrong.net>
2018-12-24 11:47:17 +0800

    3456

--- chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -10,5 +10,6 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		System.out.println("12345");
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("23467");
 	}
 }
